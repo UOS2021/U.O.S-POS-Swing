c2f413f67be50e1938a639fd17034ff436883618
UDADDY <gustmd5715@gmail.com>
2021-08-06 00:27:08 +0900

    메뉴관리패널 메뉴추가버튼  팝업창 구현. menu.json에는 저장X (추후에 구현)

--- src/main/PosGUIMenuEditPanel.java
+++ src/main/PosGUIMenuEditPanel.java
@@ -7,16 +7,21 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -28,11 +33,16 @@ public class PosGUIMenuEditPanel extends JPanel implements ActionListener, Chang
 	private CardLayout cards;
 	private JFrame frame;
 	private JButton backButton;
-	private JButton addMenuButton;
+	private JButton imgChooseButton;
+	private JButton menuSaveButton;
+	private List<JButton> addMenuButtonList;
+	
 	private Menu menu;
 	private JSONArray menuJson;
 	private JTabbedPane tp;
 	private List<JPanel> categoryPanel;
+	private JDialog addMenuDialog;
+	private JLabel chooseFoodLabel;
 	
 	public PosGUIMenuEditPanel(CardLayout cards, JFrame frame) {
 		this.setLayout(null);
@@ -47,15 +57,17 @@ public class PosGUIMenuEditPanel extends JPanel implements ActionListener, Chang
 		insertCategoryPanelIntoTabbedPanel();
 				
 		backButton = new JButton("BACK");
+		backButton.setBounds(200, 500, 100, 40);
 		backButton.addActionListener(this);
-		
-		add(tp,BorderLayout.SOUTH);
-		add(backButton,BorderLayout.NORTH);
+
+		add(tp);
+		add(backButton);
 	}
 
 	private void initializeCategoryPanel() {
 		int categorySize = menu.getCategorySize();
 		categoryPanel = new ArrayList<JPanel>();
+		addMenuButtonList = new ArrayList<JButton>();
 		
 		for(int i=0;i<categorySize;i++) {
 			JPanel menuPanel = new JPanel();
@@ -79,8 +91,10 @@ public class PosGUIMenuEditPanel extends JPanel implements ActionListener, Chang
 											
 				menuPanel.add(foodPanel);
 			}
-			addMenuButton = new JButton("+");
-			menuPanel.add(addMenuButton);
+			JButton addMenuButton = new JButton("+");
+			addMenuButton.addActionListener(this);
+			addMenuButtonList.add(addMenuButton);
+			menuPanel.add(addMenuButtonList.get(i));
 			categoryPanel.add(menuPanel);
 		}
 	}
@@ -97,9 +111,61 @@ public class PosGUIMenuEditPanel extends JPanel implements ActionListener, Chang
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		int addMenuButtonIdx = addMenuButtonList.indexOf(e.getSource()); 
 		if(e.getSource()==backButton) { // Back Button
 			cards.show(frame.getContentPane(), "mainPanel");
-		} else if(e.getSource()==addMenuButton) { // Menu Add Button
+		} else if(addMenuButtonIdx>=0) {
+			addMenuDialog = new JDialog(this.frame, "메뉴 추가", true);
+			addMenuDialog.setSize(300,500);
+			addMenuDialog.setLayout(new GridLayout(5,1));
+
+			
+			chooseFoodLabel = new JLabel();
+			imgChooseButton = new JButton("Open");
+			imgChooseButton.addActionListener(this);
+			menuSaveButton = new JButton("Save");
+			menuSaveButton.addActionListener(this);
+			JTextArea nameTextArea = new JTextArea(2, 10);
+			JTextArea descriptionTextArea = new JTextArea(2,10);
+			JTextArea priceTextArea = new JTextArea(2,10);
+			
+			JPanel foodPanel = new JPanel();
+			foodPanel.setLayout(new BorderLayout());
+			foodPanel.add(chooseFoodLabel);
+			foodPanel.add(imgChooseButton,BorderLayout.SOUTH);
+			
+			JPanel namePanel = new JPanel();
+			namePanel.setLayout(new FlowLayout());
+			namePanel.add(new JLabel("이름"),FlowLayout.LEFT);
+			namePanel.add(nameTextArea);
+			
+			JPanel pricePanel = new JPanel();
+			pricePanel.add(new JLabel("가격"));
+			pricePanel.add(priceTextArea);
+			
+			JPanel descriptionPanel = new JPanel();
+			descriptionPanel.add(new JLabel("설명"),FlowLayout.LEFT);
+			descriptionPanel.add(descriptionTextArea);
+			
+			addMenuDialog.add(foodPanel);
+			addMenuDialog.add(namePanel);
+			addMenuDialog.add(pricePanel);
+			addMenuDialog.add(descriptionPanel);
+			addMenuDialog.add(menuSaveButton);
+			
+			addMenuDialog.setVisible(true);
+		}  else if(e.getSource()==imgChooseButton) { 
+			JFileChooser fc = new JFileChooser();
+			int i= fc.showOpenDialog(this);
+			if(i==JFileChooser.APPROVE_OPTION) {
+				File f = fc.getSelectedFile();
+				String filepath = f.getPath();
+				System.out.println(filepath);
+				
+				ImageIcon icon = new ImageIcon(filepath);
+				chooseFoodLabel.setIcon(icon);
+			}
+		} else if(e.getSource()==menuSaveButton) {
 			
 		}
 	}
